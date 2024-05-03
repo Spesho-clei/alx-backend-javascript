ac59fbab0f071fa130797e47a4246c6cc7082061
Spesho-clei <jepkoechnancy56@gmail.com>
2024-05-03 17:40:33 +0000

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Holberton School, what is your name?");
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim(); // Trim whitespace and convert to string
+  console.log(`Your name is: ${name}`);
+  console.log("This important software is now closing");
+  process.exit();
+});
